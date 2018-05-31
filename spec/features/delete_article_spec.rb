54776b60981972578bd876e0f4b96cd716e2c2de
mohamed.ashour <mohamed.ashour@pharos-solutions.de>
2018-05-31 08:43:33 -0400

    Implementing delete article

---
+++ spec/features/delete_article_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "Deleting an Article" do
+  before do
+    @article=Article.create(title: "Title one",body: "Body one")
+  end
+  scenario "A user deletes an Article" do
+    visit"/"
+    click_link @article.title
+    click_link "Delete Article"
+    expect(page).to have_content("Article has been deleted")
+    expect(current_path).to eq(articles_path)
+  end
+end
